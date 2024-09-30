323c748c457bcac024d90993341e82ffcbb2ae49
Yak <yassinekhayri123@gmail.com>
2024-09-30 10:35:36 +0100

    Scroll to the latest message

--- script.js
+++ script.js
@@ -9,9 +9,11 @@ myForm.addEventListener('submit', (e) => {
 		tmp = messenger.innerHTML.replace('animate', '');
 		messenger.innerHTML = tmp + `<p class="message myMsg animate">${msg.value}</p>`
 		msg.value = '';
+		messenger.scrollTop = messenger.scrollHeight
 		setTimeout(() => {
 			tmp = messenger.innerHTML.replace('animateF', '').replace('animate', '');
 			messenger.innerHTML = tmp + `<p class="message friend animateF">Hello</p>`
+			messenger.scrollTop = messenger.scrollHeight
 		}, 1000);
 	}
 	// messenger.innerHTML = messenger.innerHTML.replace('animateF', '').replace('animate', '')

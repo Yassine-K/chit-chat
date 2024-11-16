5eda1ff6af9079cf2bd4774911a3c38dc58678e0
Oussama <osajide5@gmail.com>
2024-11-16 11:09:57 +0100

    add websocket connection

--- script.js
+++ script.js
@@ -3,18 +3,34 @@ msg = document.getElementById('msg');
 submit = document.getElementById('sub');
 myForm = document.getElementById('messenger')
 
+socket = new WebSocket("ws://localhost:8000/chat/oussama")
+
+socket.onopen = function(event) {
+	console.log('WebSocket is open now.')
+}
+
+socket.onmessage = function(event) {
+	const data = JSON.parse(event.data);
+	console.log('Message from server:', data.msg);
+};
+
+socket.onclose = function(event) {
+	console.log('WebSocket is closed now.');
+};
+
 myForm.addEventListener('submit', (e) => {
 	e.preventDefault()
 	if (msg.value) {
 		tmp = messenger.innerHTML.replace('animate', '');
 		messenger.innerHTML = tmp + `<p class="message myMsg animate">${msg.value}</p>`
+		socket.send(JSON.stringify({ 'message': `${msg.value}` }));
 		msg.value = '';
 		messenger.scrollTop = messenger.scrollHeight
-		setTimeout(() => {
-			tmp = messenger.innerHTML.replace('animateF', '').replace('animate', '');
-			messenger.innerHTML = tmp + `<p class="message friend animateF">Hello</p>`
-			messenger.scrollTop = messenger.scrollHeight
-		}, 1000);
+		// setTimeout(() => {
+		// 	tmp = messenger.innerHTML.replace('animateF', '').replace('animate', '');
+		// 	messenger.innerHTML = tmp + `<p class="message friend animateF">Hello</p>`
+		// 	messenger.scrollTop = messenger.scrollHeight
+		// }, 1000);
 	}
 	// messenger.innerHTML = messenger.innerHTML.replace('animateF', '').replace('animate', '')
 })
